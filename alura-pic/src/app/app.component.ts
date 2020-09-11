aee4eab0f3c311d8835e0a1f17b589886a5fddad
Thiago Tavares <tvttavares@gmail.com>
2020-09-10 23:08:35 -0400

    Adicionando api backend

--- alura-pic/src/app/app.component.ts
+++ alura-pic/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'alura-pic';
+
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    }
+  ];
 }
