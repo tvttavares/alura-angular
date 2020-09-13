7bbf7223d62e8143df5382f76dbbaa5c698d8019
Thiago Tavares <tvttavares@gmail.com>
2020-09-13 12:33:19 -0400

    Componente exclusivo para listar fotos

--- alura-pic/src/app/photos/photos.module.ts
+++ alura-pic/src/app/photos/photos.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotosListComponent } from './photos-list/photos-list.component';
 import { PhotosFormComponent } from './photos-form/photos-form.component';
+import { PhotosComponent } from './photos-list/photos/photos.component';
 
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotosListComponent,
-    PhotosFormComponent
+    PhotosFormComponent,
+    PhotosComponent
   ],
   imports: [
     CommonModule,
