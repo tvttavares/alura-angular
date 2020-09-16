6adea36846223273287f45bde1729e1beb711e18
Thiago Tavares <tvttavares@gmail.com>
2020-09-16 11:11:40 -0400

    Adicao biblioteca Font Awesome e componente compartilhado(shared)

--- alura-pic/src/app/photos/photos-list/photos-list.module.ts
+++ alura-pic/src/app/photos/photos-list/photos-list.module.ts
@@ -7,6 +7,7 @@ import { PhotosComponent } from './photos/photos.component';
 import { PhotosListComponent } from './photos-list.component';
 
 import { PhotoModule } from './../photo/photo.module';
+import { CardModule } from './../../shared/components/card/card.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { PhotoModule } from './../photo/photo.module';
   ],
   imports: [
     CommonModule,
-    PhotoModule
+    PhotoModule,
+    CardModule
   ]
 })
 export class PhotosListModule { }
