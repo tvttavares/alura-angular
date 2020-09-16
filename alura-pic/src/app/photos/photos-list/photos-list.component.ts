e6a97e9b007658556c3093f8b97a9c054318e27a
Thiago Tavares <tvttavares@gmail.com>
2020-09-15 23:36:15 -0400

    RxJS e seu Subject: Melhorando performance de filtro

--- alura-pic/src/app/photos/photos-list/photos-list.component.ts
+++ alura-pic/src/app/photos/photos-list/photos-list.component.ts
@@ -1,5 +1,8 @@
+import { OnDestroy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -8,17 +11,24 @@ import { Photo } from '../photo/photo';
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
-export class PhotosListComponent implements OnInit {
+export class PhotosListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute) { }
 
+
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
-
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
