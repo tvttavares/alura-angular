420289059390c6c482711db6178c32386a3fdc4d
Thiago Tavares <tvttavares@gmail.com>
2020-10-08 23:16:37 -0400

    Interceptadores e envio do token

--- alura-pic/src/app/core/core.module.ts
+++ alura-pic/src/app/core/core.module.ts
@@ -1,8 +1,10 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { HeaderComponent } from './header/header.component';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
   declarations: [HeaderComponent],
@@ -10,6 +12,13 @@ import { HeaderComponent } from './header/header.component';
     CommonModule,
     RouterModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule { }
